0f2e60812467c40efba3e8bbf9ebc2cda9ea8ac2
MaulanaArya30 <maulanaaryaw@gmail.com>
2023-08-18 14:39:13 +0700

    mobile first prototype

--- src/app/RPLD/home_page/page.jsx
+++ src/app/RPLD/home_page/page.jsx
@@ -1,15 +1,48 @@
 'use client';
 
+import Image from 'next/image'
+import yellowdots from '../../../components/images/yellowdots.png'
+import logo from '../../../components/images/logo.png'
+
 export default function HomePage() {
     return (
-        <div className="p-10">
-            <h1 className="text-3xl font-bold">Lab Rekayasa Perangkat Lunak dan Data</h1>
-
-            <div className='mt-[60px] grid grid-cols-2 place-items-center'>
-                <button onClick={() => alert('Hello World!')}>Log In</button>
-                <button onClick={() => alert('Bye bye World!')}>Log Out</button>
+        <div className="p-[18px] bg-white h-screen w-screen">
+           
+            <Image 
+                src={yellowdots}
+                alt="Main Screen"
+                objectFit='contain'
+            />
+            <div className="mt-[51px] h-7 text-center text-gray-800 text-xl font-medium leading-snug drop-shadow-lg">Halo! <br/>Kamu berada di</div>
+            <div className='pt-[75px] flex justify-center items-center'>
+            <Image 
+                src={logo}
+                alt="Main Screen"
+                width={170}
+                height={170}
+            />
             </div>
+            <div className="h-10 pt-[24px] text-center text-gray-800 text-2xl font-extrabold leading-10 drop-shadow-xl">Lab Rekayasa Perangkat Lunak dan Data</div>
+            
+            
+
+            <div className="mt-[100px] h-12 relative flex justify-center" onClick={() => alert('Kamu Anggota')}>
+            <div className="w-60 h-12 left-0 top-0 bg-teal-600 rounded-3xl" />
+            <div className="Anggota top-[10px] absolute text-center text-white text-lg font-normal">ANGGOTA</div>
+            </div>
+            <div className="mt-4  h-12 relative flex justify-center" onClick={() => alert('Kamu Bukan Anggota')}>
+            <div className=" w-60 h-12 left-0 top-0 bg-teal-600 rounded-3xl" />
+            <div className="NonAnggota top-[10px] absolute text-center text-white text-lg font-normal">NON-ANGGOTA</div>
+            </div>
+
+            
+            {/* <div className='mt-[85px] grid grid-cols-2 place-items-center text-black'>
+                
+                <button onClick={() => alert('Bye bye World!')}>Log Out</button>
+            </div> */}
+            
             
         </div>
+        
     )
 }
