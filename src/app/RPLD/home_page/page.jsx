4226d18159c354f4c8a383e39a3db55b6b696f62
MaulanaArya30 <maulanaaryaw@gmail.com>
2023-08-19 21:39:02 +0700

    main page v1

--- src/app/RPLD/home_page/page.jsx
+++ src/app/RPLD/home_page/page.jsx
@@ -3,6 +3,7 @@
 import Image from 'next/image'
 import yellowdots from '../../../components/images/yellowdots.png'
 import logo from '../../../components/images/logo.png'
+import Link from 'next/link'
 
 export default function HomePage() {
     return (
@@ -25,21 +26,20 @@ export default function HomePage() {
             <div className="h-10 pt-[24px] text-center text-gray-800 text-2xl font-extrabold leading-10 drop-shadow-xl">Lab Rekayasa Perangkat Lunak dan Data</div>
             
             
-
-            <div className="mt-[100px] h-12 relative flex justify-center" onClick={() => alert('Kamu Anggota')}>
+            <Link href="/RPLD/anggota_page">
+            <div className="mt-[100px] h-12 relative flex justify-center">
             <div className="w-60 h-12 left-0 top-0 bg-teal-600 rounded-3xl" />
             <div className="Anggota top-[10px] absolute text-center text-white text-lg font-normal">ANGGOTA</div>
             </div>
-            <div className="mt-4  h-12 relative flex justify-center" onClick={() => alert('Kamu Bukan Anggota')}>
+            </Link>
+
+           
+            <Link href="/RPLD/nonanggota_page">
+            <div className="mt-4  h-12 relative flex justify-center">
             <div className=" w-60 h-12 left-0 top-0 bg-teal-600 rounded-3xl" />
             <div className="NonAnggota top-[10px] absolute text-center text-white text-lg font-normal">NON-ANGGOTA</div>
             </div>
-
-            
-            {/* <div className='mt-[85px] grid grid-cols-2 place-items-center text-black'>
-                
-                <button onClick={() => alert('Bye bye World!')}>Log Out</button>
-            </div> */}
+            </Link>
             
             
         </div>
