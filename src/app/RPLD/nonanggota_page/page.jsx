721f4acfc614511175b7b1226d81813230ce741b
BagusAlwan <bgslwn28@gmail.com>
2023-09-14 18:59:19 +0700

    almost there

--- src/app/RPLD/nonanggota_page/page.jsx
+++ src/app/RPLD/nonanggota_page/page.jsx
@@ -7,88 +7,91 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 
 export default function NonAnggotaPage() {
-  const [nama, setNama] = useState("");
-  const [nim, setNim] = useState("");
+    const [nama, setNama] = useState("");
+    const [nim, setNim] = useState("");
 
-  const date = new Date();
+    const date = new Date();
 
-  const makeApiCall = async () => {
-    await fetch('/api/sheets', {
-      method: 'POST',
-      body: JSON.stringify({ nim: nim, nama: nama, action: "IN", waktu: date })
-    })
-  }
+    const handleNamaChange = (e) => {
+        setNama(e.target.value);
+    };
 
-  const handleNamaChange = (e) => {
-    setNama(e.target.value);
-  };
-
-  const handleNimChange = (e) => {
-    setNim(e.target.value);
-  };
+    const handleNimChange = (e) => {
+        setNim(e.target.value);
+    };
 
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log("Nama:", nama);
-  //   console.log("NIM:", nim);
-  //   alert(nama + " " + nim);
-  //   addRowToSheet(nim, nama, "IN", date)
-  // };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("Nama:", nama);
+        console.log("NIM:", nim);
+        const action = "IN";
+        const date = new Date().toLocaleTimeString();
 
-  return (
-    <div className="p-[18px] bg-white h-screen w-screen overflow-auto">
-      <Image src={yellowdots} alt="Main Screen" objectFit="contain" />
-      <div className="mt-[35%] h-7 text-center text-gray-800 text-xl font-medium leading-snug drop-shadow-lg">
-        Masukan Data Berikut :{" "}
-      </div>
-      <div className="pt-[45px] flex justify-center items-center">
-        <form onSubmit={makeApiCall}>
-          <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900">
-              Nama
-            </label>
-            <input
-              type="text"
-              placeholder="Jane doe"
-              value={nama}
-              onChange={handleNamaChange}
-              className="placeholder-gray-400 p-2 text-gray-900 border-b-2 border-gray-300 bg-white palceholder:"
-            ></input>
-            <label className="mt-6 block mb-2 text-sm font-medium text-gray-900">
-              NIM
-            </label>
-            <input
-              type="text"
-              placeholder="2X/XXXXX/XX/XXXXXXX"
-              value={nim}
-              onChange={handleNimChange}
-              className="placeholder-gray-400 p-2 text-gray-900 border-b-2 border-gray-300 bg-white palceholder:"
-            ></input>
-          </div>
-        </form>
-      </div>
+        const res = await fetch('/api/sheets', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nama, nim, action, date
+            })
+        })
+    };
 
-      <div
-        className="mt-[100px] h-12 relative flex justify-center "
-        // onClick={() => }
-        onClick={makeApiCall}
-        style={{ cursor: "pointer" }}
-      >
-        <div className="w-60 h-12 left-0 top-0 bg-teal-600 rounded-3xl" />
-        <div className="Anggota top-[10px] absolute text-center text-white text-lg font-normal">
-          MASUK
+    return (
+        <div className="p-[18px] bg-white h-screen w-screen overflow-auto">
+            <Image src={yellowdots} alt="Main Screen" objectFit="contain" />
+            <div className="mt-[35%] h-7 text-center text-gray-800 text-xl font-medium leading-snug drop-shadow-lg">
+                Masukan Data Berikut :{" "}
+            </div>
+            <div className="pt-[45px] flex justify-center items-center">
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-gray-900">
+                            Nama
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Jane doe"
+                            value={nama}
+                            onChange={handleNamaChange}
+                            className="placeholder-gray-400 p-2 text-gray-900 border-b-2 border-gray-300 bg-white palceholder:"
+                        ></input>
+                        <label className="mt-6 block mb-2 text-sm font-medium text-gray-900">
+                            NIM
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="2X/XXXXX/XX/XXXXXXX"
+                            value={nim}
+                            onChange={handleNimChange}
+                            className="placeholder-gray-400 p-2 text-gray-900 border-b-2 border-gray-300 bg-white palceholder:"
+                        ></input>
+                    </div>
+                </form>
+            </div>
+
+            <div
+                className="mt-[100px] h-12 relative flex justify-center "
+                // onClick={() => }
+                onClick={handleSubmit}
+                style={{ cursor: "pointer" }}
+            >
+                <div className="w-60 h-12 left-0 top-0 bg-teal-600 rounded-3xl" />
+                <div className="Anggota top-[10px] absolute text-center text-white text-lg font-normal">
+                    MASUK
+                </div>
+            </div>
+
+            <Link href="/RPLD/alat_page">
+                <div className="mt-4  h-12 relative flex justify-center">
+                    <div className=" w-60 h-12 left-0 top-0 bg-teal-600 rounded-3xl" />
+                    <div className="NonAnggota top-[10px] absolute text-center text-white text-lg font-normal">
+                        KELUAR
+                    </div>
+                </div>
+            </Link>
         </div>
-      </div>
-
-      <Link href="/RPLD/alat_page">
-        <div className="mt-4  h-12 relative flex justify-center">
-          <div className=" w-60 h-12 left-0 top-0 bg-teal-600 rounded-3xl" />
-          <div className="NonAnggota top-[10px] absolute text-center text-white text-lg font-normal">
-            KELUAR
-          </div>
-        </div>
-      </Link>
-    </div>
-  );
+    );
 }
--- src/app/api/sheets/route.js
+++ src/app/api/sheets/route.js
@@ -1,19 +1,11 @@
-import { addRowToSheet } from '@/server/sheets.mjs'; // Import your addRowToSheet function
+import { addRowToSheet } from "@/server/sheets.mjs";
+import { NextResponse } from "next/server";
 
-export async function POST(req) {
-  const body = await req.body;
-  console.log(body);
-
-  // Extract the data from the request body
-  const { nim, nama, action, waktu } = body;
-
-  try {
-    // Call addRowToSheet with the extracted data
-    await addRowToSheet(nim, nama, action, waktu);
-    console.log('Row added successfully');
-    return new Response('OK');
-  } catch (error) {
-    console.error('Error adding row:', error);
-    return new Response('Error', { status: 500 });
-  }
+export async function POST(request){
+  const data = await request.json()
+  console.log(data)
+  const {nama, nim, action, date} = data
+  addRowToSheet(nama, nim, action, date)
+  
+  return NextResponse.json({data})
 }
