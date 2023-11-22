49c3fce576f1680db7eed8eef9e31be07b8c5601
BagusAlwan <bgslwn28@gmail.com>
2023-11-22 19:43:05 +0700

    donzo fe

--- src/app/SC/alat_page/page.jsx
+++ src/app/SC/alat_page/page.jsx
@@ -12,7 +12,7 @@ export default function AlatPage() {
   const nim = searchParams.get("nim");
   const lab = searchParams.get("lab");
   const xhr = new XMLHttpRequest();
-  
+
   const [selectedOption, setSelectedOption] = useState("Alat Pribadi");
   const handleSelectChange = (e) => {
     setSelectedOption(e.target.value);
@@ -49,11 +49,44 @@ export default function AlatPage() {
     }
   };
 
+  const [visitorData, setVisitorData] = useState([]);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/alatData/group/${lab}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setVisitorData(data);
+        console.log("Fetched Data:", data); // Log the fetched data
+      } else {
+        console.error("Failed to fetch data");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(); // Fetch data when the component mounts
+
+    const intervalId = setInterval(fetchData, 5000); // Fetch data every 5 seconds
+
+    return () => clearInterval(intervalId); // Clean up the interval on unmount
+  }, []);
+
   return (
     <div className="p-[18px] bg-white h-screen w-screen overflow-auto">
       <Image src={yellowdots} alt="Main Screen" objectFit="contain" />
       <div className="mt-[100px] md:mt-0 lg:mt-[100px]     h-7 text-center text-gray-800 text-xl font-medium leading-snug drop-shadow-lg">
-        Pilih alat yg digunakan :{" "}
+        Pilih alat yg digunakan :
       </div>
       <div className="pt-[75px] flex justify-center items-center">
         <select
@@ -61,19 +94,18 @@ export default function AlatPage() {
           onChange={handleSelectChange} // Call handleSelectChange on select change
           value={selectedOption} // Set the selected value based on the state
         >
-          <option value="Alat Pribadi">Alat Pribadi</option>
-          <option value="Layar Monitor">Layar Monitor</option>
-          <option value="Lcd layar lebar">Lcd layar lebar</option>
-          <option value="Pc komputer">Pc komputer</option>
-          <option value="NAS">NAS</option>
-          <option value="Mac mini ">Mac mini</option>
-          <option value="Printer">Printer</option>
+          {visitorData.map((item) => (
+            <option key={item.id} value={item.alat}>
+              {item.alat}
+            </option>
+          ))}
         </select>
       </div>
 
       <div
         className="mt-[100px]  h-12 relative flex justify-center"
         onClick={handleSubmitAlat}
+        style={{ cursor: "pointer" }}
       >
         <div className=" w-60 h-12 left-0 top-0 bg-teal-600 rounded-3xl" />
         <div className="NonAnggota top-[10px] absolute text-center text-white text-lg font-normal">
